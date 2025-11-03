c55c29bbc383304caa650e0976e3cd7aa280fb04
adela miclea <adelamiclea709@yahoo.com>
2025-11-03 19:18:31 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import './App.css';
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import SearchIcon from '@mui/icons-material/Search';
+import "./Header.js"
+import Header from './Header.js';
+
+
 function App() {
   return (
     <div className="App">
-      <div className="header_logo">
-        <StorefrontIcon fontSize='large' className='header_logoImage'/>
-        <h2 className="header_logoTitle">eShop</h2>
-      </div>
-
-      <div className="header_searchbar">
-        <input type="text" className='header_searchInput' />
-        <SearchIcon fontSize='large' className='header_searchIcon'/>
-      </div>
-      
-      <div className="header_nav">
-        <div className="nav_item">
-          <span className="nav_itemLineOne">Hello guest</span>
-          <span className="nav_itemLineTwo">Sign in</span>
-        </div>
-        <div className="nav_item">
-          <span className="nav_itemLineOne">Your</span>
-          <span className="nav_itemLineTwo">Shop</span>
-        </div>
-        <div className="nav_item">
-          <ShoppingBasketIcon fontSize='large'/>
-          <span className="nav_itemLineTwo">0</span>
-        </div>
-      </div>
+     <Header/>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,1 +1,38 @@
-_
+import React from "react";
+import "./Header.css";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header(){
+    return (
+        <div className="header">
+             <div className="header_logo">
+        <StorefrontIcon fontSize='large' className='header_logoImage'/>
+        <h2 className="header_logoTitle">eShop</h2>
+      </div>
+
+      <div className="header_searchbar">
+        <input type="text" className='header_searchInput' />
+        <SearchIcon fontSize='large' className='header_searchIcon'/>
+      </div>
+      
+      <div className="header_nav">
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Hello guest</span>
+          <span className="nav_itemLineTwo">Sign in</span>
+        </div>
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Your</span>
+          <span className="nav_itemLineTwo">Shop</span>
+        </div>
+        <div className="nav_item">
+          <ShoppingBasketIcon fontSize='large' className="itemBasket"/>
+          <span className="nav_itemLineTwo nav_basketCount">0</span>
+        </div>
+      </div>
+        </div>
+    )
+}
+
+export default Header
