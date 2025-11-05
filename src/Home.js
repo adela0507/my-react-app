6baf004cdede986b33faf8cb69a605ea6d4f4c2a
adela miclea <adelamiclea709@yahoo.com>
2025-11-05 20:34:33 +0200

    props

--- src/Home.js
+++ src/Home.js
@@ -10,13 +10,48 @@ function Home(){
                     <img src="https://wallpapers.com/images/high/e-commerce-pictures-sgogpik1xs1aof4c.webp" alt="image" className="home_image" />
 
                     <div className="home_row">
-                        <Product/>
-                        <Product/>
+                        <Product 
+                        id="1234"
+                        title="UGG
+Ghete slip-on de piele intoarsa cu finisaj rezistent la apa Classic Ultra Mini"
+                        price={100}
+                        rating={5}
+                        image="https://fdcdn.akamaized.net/m/780x1170/products/96358/96357790/images/res_8cc9ea1326669725893297a9470e3687.jpg?s=3HlDpJPDPVSx"
+                        />
+                        <Product
+                         id="12349"
+                        title="Valentino Bags
+Geanta shopper din piele ecologica Zero"
+                        price={120}
+                        rating={4}
+                        image="https://fdcdn.akamaized.net/m/780x1170/products/94481/94480691/images/res_33152e9ad8e227b5b189887de1de37ff.jpg?s=FTAAdHwRwSBq"
+                       />
                     </div>
                     <div className="home_row">
-                        <Product/>
-                        <Product/>
-                        <Product/>
+                        <Product
+                         id="12347"
+                        title="GUESS
+Rochie-pulover cu pliuri"
+                        price={60}
+                        rating={3}
+                        image="https://fdcdn.akamaized.net/m/780x1170/products/95111/95110401/images/res_13e709429c39617fa03091715c563d1e.jpg?s=eipT9QGagnKF"
+                       />
+                        <Product
+                         id="12346"
+                        title="GUESS
+Ghete de piele ecologica"
+                        price={90}
+                        rating={5}
+                        image="https://fdcdn.akamaized.net/m/780x1170/products/71986/71985530/images/res_4e1a917305f39a44ba8e58851869fcaf.jpg?s=7aJjD5w42PXg"
+                       />
+                        <Product
+                         id="12345"
+                        title="Karl Lagerfeld
+Pantofi sport din piele cu aplicatie logo metalica"
+                        price={110}
+                        rating={3}
+                        image="https://fdcdn.akamaized.net/m/780x1170/products/51852/51851787/images/res_3d4c273c79fcbf8fe79bde14710195d0.jpg?s=vaYSXZ96pWXQ"
+                       />
                        
                     </div>
                     <div className="home_row"></div>
--- src/Product.js
+++ src/Product.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Product.css";
 
-function Product(){
+function Product({id,price,rating,title,image}){
     return (
         <div className="product">
             <div className="product_info">
-                <p>Title</p>
-                <p className="product_price">$30</p>
-                <div className="product_rating">Stars</div>
+                <p>{title}</p>
+                <p className="product_price">${price}</p>
+                <div className="product_rating">
+                    {Array(rating)
+                    .fill()
+                        .map((_, i)=>(
+                        <p>💖</p>
+                        )) }
+                    </div>
             </div>
-            <img src="https://th.bing.com/th?id=OPEC.wcGy3%2busQ%2fc5%2bA474C474&w=592&h=550&o=5&dpr=1.5&pid=21.1" alt="product" />
+            <img src={image} alt="product" />
             <button>Add to Basket</button>
         </div>
     )
