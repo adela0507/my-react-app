87fbd684935c87ba104870c5eafeada14a6f2eaa
adela miclea <adelamiclea709@yahoo.com>
2025-11-11 17:51:30 +0200

    add_to_basket

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,17 +2,21 @@ import React from "react";
 import "./CheckoutProduct.css";
 import Subtotal from "./Subtotal";
 
-function CheckoutProduct(){
+function CheckoutProduct({id,title,price,rating,image}){
     return(
         <div className="checkoutProduct">
-            <img src="https://fdcdn.akamaized.net/m/780x1170/products/96358/96357790/images/res_8cc9ea1326669725893297a9470e3687.jpg?s=3HlDpJPDPVSx" alt="" className="checkoutProduct_image" />
+            <img src={image} alt="" className="checkoutProduct_image" />
             <div className="checkoutProduct_info">
-                <div className="checkoutProduct_Title"><p>UGG Ghete slip-on de piele intoarsa cu finisaj rezistent la apa Classic Ultra Mini</p></div>
+                <div className="checkoutProduct_Title"><p>{title}</p></div>
                 <p className="checkoutPrice">
                     <small>$</small>
-                    <strong>100</strong>
+                    <strong>{price}</strong>
                     </p>
-                <div className="checkoutProduct_rating">💖💖</div>
+                <div className="checkoutProduct_rating">{Array(rating)
+                    .fill()
+                        .map((_, i)=>(
+                        <p>💖</p>
+                        )) }</div>
                 <button className="checkoutRemove">Remove from basket</button>
 
             </div>
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,19 +1,22 @@
 import React from "react";
 import CurrencyInput from "react-currency-input-field";
 import "./Subtotal.css";
+import { useStateValue } from "./StateProvider";
+import { getBasketTotal } from "./reducer";
 
 function Subtotal() {
+  const[{basket},dispatch]=useStateValue();
   return (
     <div className="subtotal">
       <CurrencyInput
-        value={0}
+        value={getBasketTotal(basket)}
         readOnly
         prefix="$"
         decimalsLimit={2}
         className="subtotal_value"
       />
       
-      <p>Subtotal (0 items)</p>
+      <p>Subtotal ({basket.length})</p>
       <small className="subtotal_gift">
         <input type="checkbox" /> This order contains a gift
       </small>
